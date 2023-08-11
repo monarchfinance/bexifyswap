fef85c16b40ae787de1af7b6e367966f942c1f3f
Monarch Finance <monarch.finance.official@gmail.com>
2023-08-11 15:24:58 +0200

    init

---
+++ src/constants/lists.ts
@@ -0,0 +1,8 @@
+// const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json';
+const TESTNET_LIST = 'https://raw.githubusercontent.com/ponizhand/tokens-list/main/testnet.json'
+
+// lower index == higher priority for token import
+export const DEFAULT_LIST_OF_LISTS: string[] = [TESTNET_LIST];
+
+// default lists to be 'active' aka searched across
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [TESTNET_LIST];
