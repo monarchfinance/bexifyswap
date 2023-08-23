7df6df7929ebe7a86c1b3d7447dec4f430ae26b2
Monarch Finance <monarch.finance.official@gmail.com>
2023-08-23 13:07:06 +0200

    change router

--- src/pages/AddLiquidity/index.tsx
+++ src/pages/AddLiquidity/index.tsx
@@ -115,8 +115,12 @@ export default function AddLiquidity({
   const addTransaction = useTransactionAdder();
 
   async function onAdd() {
+
+    console.log("add liq")
     if (!chainId || !library || !account) return;
     const router = getRouterContract(chainId, library, account);
+
+    console.log(router)
 
     const { [Field.CURRENCY_A]: parsedAmountA, [Field.CURRENCY_B]: parsedAmountB } = parsedAmounts;
     if (!parsedAmountA || !parsedAmountB || !currencyA || !currencyB || !deadline) {
